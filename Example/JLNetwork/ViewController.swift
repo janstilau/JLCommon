669d8710f32ea914a1b421badc2ad9a46d40c849
liuguoqiang <liugq01@rd.netease.com>
2022-02-13 23:19:09 +0800

    sync

--- Example/JLNetwork/ViewController.swift
+++ Example/JLNetwork/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import JLNetwork
 
 class ViewController: UIViewController {
 
@@ -14,10 +15,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+     
+        NetworkManager.Get("The Url")
     }
 
 }
---
+++ JLNetwork/Classes/Netowrk.swift
@@ -0,0 +1,19 @@
+//
+//  Netowrk.swift
+//  JLNetwork
+//
+//  Created by JustinLau on 2022/2/13.
+//
+
+import UIKit
+
+open class NetworkManager: NSObject {
+    
+    open static func Get(_ url: String) -> Void {
+        print("Get")
+    }
+    
+    open static func Post(_ url: String) -> Void {
+        print("Post")
+    }
+}
